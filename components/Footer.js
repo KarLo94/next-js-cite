d57739a50550a2a3a37569174fab8adaf2e2a2cc
KarLo94 <karpenko.l94@mail.ru>
2023-10-26 16:45:44 +0300

    start footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,42 @@
-const Footer = () => (
-  <p> Тут будет футер</p>
-);
- 
+import styles from "../styles/Footer.module.scss";
+
+const Footer = () => {
+  return (
+    
+    <div>
+      
+      {/* // Картинка и ссылки */}
+      <div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+          
+        </div>
+      </div>
+      
+      
+      {/* // Подписка с формой */}
+      <div>
+
+      </div>
+
+      {/* Навигации */}
+      <div>
+
+      </div>
+
+      {/* Копирайт */}
+      <div>
+
+      </div>
+
+
+    </div>
+  );
+};
 
 export default Footer;
