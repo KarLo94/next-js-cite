63acf22339ce4330f0e675ddc3f25bd35b479fcb
KarLo94 <karpenko.l94@mail.ru>
2023-11-07 12:17:14 +0300

    swiper

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Heading from '../components/Heading'
+
+import { Swiper, SwiperSlide} from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import styles from '@/styles/Home.module.scss'
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 
 
 export default function Home() {
   return (
-    <>
+
+     <>
       <Head>
         <title>ЭТО МОЙ Hoff: гипермаркет мебели и товаров для дома, интернет-магазин мебели</title>
         <meta name="description" content="Мебель и товары для дома в интернет-магазине HOFF. 🚚 Быстрая доставка по Москве, Санкт-Петербургу и всей России. Широкий ассортимент, более 50 000 товаров ✔️ Самовывоз товаров для дома из наших магазинов или пункта выдачи в вашем городе (для заказов весом до 20 кг)" />
@@ -16,7 +24,31 @@ export default function Home() {
       </Head>
       <main className={`${styles.main}`}>
         <div className={styles.main__banner}>
-
+          <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+              delay: 6000,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,}}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            className="mySwiper">
+            <SwiperSlide><a href='#'><Image src='/image-index/2000x676_75.webp'
+              width={1499} height={506} alt='swiper image' /></a></SwiperSlide>
+            <SwiperSlide><a href='#'><Image src='/image-index/2000x676_75(1).webp'
+              width={1499} height={506} alt='swiper image' /></a></SwiperSlide>
+            <SwiperSlide><a href='#'><Image src='/image-index/2000x676_75(2).webp'
+              width={1499} height={506} alt='swiper image' /></a></SwiperSlide>
+            <SwiperSlide><a href='#'><Image src='/image-index/2000x676_75(3).webp'
+              width={1499} height={506} alt='swiper image' /></a></SwiperSlide>
+            <SwiperSlide><a href='#'><Image src='/image-index/2000x676_75(4).webp'
+              width={1499} height={506} alt='swiper image' /></a></SwiperSlide>
+            <SwiperSlide><a href='#'><Image src='/image-index/2000x676_75(5).webp'
+              width={1499} height={506} alt='swiper image' /></a></SwiperSlide>
+          </Swiper>
         </div>
       </main>
     </>
