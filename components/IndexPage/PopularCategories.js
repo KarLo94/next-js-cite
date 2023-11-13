88c9d2ef5ce2a96ff85951a02d03b2e7603e309b
KarLo94 <karpenko.l94@mail.ru>
2023-11-13 12:00:44 +0300

    13.11

---
+++ components/IndexPage/PopularCategories.js
@@ -0,0 +1,20 @@
+import Heading from "../Heading";
+import styles from "./PopCat.module.scss";
+import PopCatItem from "./PopCatItem";
+
+const PopularCategories = () => {
+  return (
+    <div className={styles.sectionPopCat}>
+      <Heading tag="h2" text="Популярные категории" />
+
+      <div className={styles.popCatFlex}>
+
+        <PopCatItem />
+
+      </div>
+
+    </div>
+  )
+}
+
+export default PopularCategories;
