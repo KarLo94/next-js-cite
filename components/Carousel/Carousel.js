cae0a720589f8a7087fc1b8169d892de11dfdcae
KarLo94 <karpenko.l94@mail.ru>
2023-11-13 21:37:11 +0300

    add swiper in carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,7 +1,14 @@
 import styles from "././Carousel.module.scss";
 import Heading from "../Heading";
-
 import Card from "../Cards/Card";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { SwiperNavBtns } from './../Swiper/SwiperNavBtns';
+
+import 'swiper/scss';
+
+
+import {Navigation } from 'swiper/modules';
+
 
 const Carousel = () => {
   return (
@@ -9,7 +16,59 @@ const Carousel = () => {
       <Heading tag="h2" text="Начните покупать" />
 
       <div className={styles.sectionCarousel}>
-        <Card />
+
+        <Swiper
+          slidesPerView={6}
+          spaceBetween={20}
+          centeredSlides={true}
+          loop={false}
+          navigation={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            1440: {
+              slidesPerView: 5,
+              spaceBetween: 20,
+            },
+            1600: {
+              slidesPerView: 5,
+              spaceBetween: 20,
+            },
+            1920: {
+              slidesPerView: 6,
+              spaceBetween: 30,
+            },
+          }}
+          
+          modules={[Navigation]} className="mySwiper">
+
+        <SwiperSlide ><Card /></SwiperSlide>
+        <SwiperSlide><Card /></SwiperSlide>
+        <SwiperSlide ><Card /></SwiperSlide>
+        <SwiperSlide><Card /></SwiperSlide>
+        <SwiperSlide ><Card /></SwiperSlide>
+        <SwiperSlide><Card /></SwiperSlide>
+        <SwiperSlide ><Card /></SwiperSlide>
+        <SwiperSlide><Card /></SwiperSlide>
+        <SwiperSlide ><Card /></SwiperSlide>
+        <SwiperSlide><Card /></SwiperSlide>
+
+
+      <SwiperNavBtns />
+      
+    </Swiper>
+
+        
       </div>
     </div>
   )
